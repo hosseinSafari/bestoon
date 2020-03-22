a89f0d2464b09fc49c70bc6345cf5087290cecd5
hossein Safari <hossein.sch75@gmail.com>
2020-03-22 15:07:36 +0430

    Expense model is there

--- bestoon/settings.py
+++ bestoon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web'
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC' #Asia/Tehran
 
 USE_I18N = True
 
